fb4a3b142522135857201f1bfc91de8d8e0564b0
Kenay16 <166553141+Kenay16@users.noreply.github.com>
2024-06-06 16:53:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let nomedapessoa = document.getElementById("nome")
+let resposta = document.getElementById("resposta")
+
+function Enviar()
+{
+    resposta.innerHTML = alert("olá Kenay tudo bem?")
+}
+
+function converterParaMetros()  
+{
+    let centímetros = parseFloat (document.getElementById)("centímetros").value;
+
+    let metros = centímetros / 100;
+
+    document.getElementById('resultado').innerHTML =
+    centímetros + "centímetros é igual a " + metros;
+}
+
+
+
+  
+
+
+
+a = 1
+while(a <= 100)
+    {
+        console.log(a)
+        a = a +1
+    }
+
+    
+
+    for ( let b = 50; b >=0; b -=5)
+        {
+            console.log(b);
+        }
+        
+      for ( let  i = 100; i >= 0; i -=10)
+        
+            {
+                console.log(i);
+                
+            }
+
+           
+
+
